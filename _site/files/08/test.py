1e5e1d9ad210ba34554ea6d9c13caee5ade1f823
Maxim Romanov <romanov.maxim@gmail.com>
2018-05-14 11:08:04 +0200

    Lesson on XML and XML-ed documents

---
+++ _site/files/08/test.py
@@ -0,0 +1,33 @@
+import re
+
+text0 = """
+<note>
+  <to>Tove</to>
+  <from>Jani</from>
+  <heading>Reminder</heading>
+  <body>Don't forget me this weekend!</body>
+</note>
+"""
+
+# EXAMPLE 1 - find/replace
+
+text = re.sub("<[^<]+>", "", text0)
+
+print(text)
+
+
+# EXAMPLE 2 - split
+
+results = re.split("</[^<]+>", text0)
+
+for r in results:
+    print(r)
+
+print(results)
+
+
+# EXAMPLE 3 - capture
+
+text = re.search(r"<from>([^<]+)</from>", text0).group(1)
+
+print(text)
